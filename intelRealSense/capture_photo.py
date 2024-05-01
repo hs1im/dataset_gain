ccff1eb8b45387fbfe413f0b4b027e8d115e50bb
Ast@tin <4922865@gmail.com>
2024-05-01 21:41:10 +0900

    finish to make image capture program

--- intelRealSense/capture_photo.py
+++ intelRealSense/capture_photo.py
@@ -5,15 +5,18 @@ import keyboard
 
 # data label
 numbers=['0','1','2','3','4','5','6','7','8','9']
+numbers_index=0
 distance=['10cm','15cm','20cm','25cm','30cm']
+distance_index=0
 degree=['+45deg','+30deg','+15deg','0deg','-15deg','-30deg','-45deg']
+degree_index=0
 
 
 def photo_status(toShow: str):
 
     # Manual text
-    manual1 = "A : number, S : distance, D : degree"
-    manual2 = "Space : capture, Q : quit"
+    manual1 = "keypad : number, Q ~ T : distance, A ~ J : degree"
+    manual2 = "Space : capture, ESC : quit"
 
     # Create a black image
     backGround = np.zeros((128,512,3), np.uint8)
@@ -39,8 +42,25 @@ def photo_status(toShow: str):
     # Display the image
     cv2.imshow("file status", image)
 
+def change_status():
+    global numbers, distance, degree,numbers_index,distance_index,degree_index
+    # key maps
+    distance_map = ['q','w','e','r','t']
+    degree_map = ['a','s','d','f','g','h','j']
+    # Change the status
+    for i in range(10):
+        if keyboard.is_pressed(str(i)):
+            numbers_index = i
+    for i in range(5):
+        if keyboard.is_pressed(distance_map[i]):
+            distance_index = i
+    for i in range(7):
+        if keyboard.is_pressed(degree_map[i]):
+            degree_index = i
+
 def capture_photo():
-    # Configure depth and color streams
+    global numbers, distance, degree
+    # Configure realsense color streams
     pipeline = rs.pipeline()
     config = rs.config()
     config.enable_stream(rs.stream.color, 1280, 720, rs.format.bgr8, 30)
@@ -48,34 +68,42 @@ def capture_photo():
     # Start streaming
     pipeline.start(config)
 
-    # Wait for a coherent pair of frames: depth and color
-    frames = pipeline.wait_for_frames()
-    depth_frame = frames.get_depth_frame()
-    color_frame = frames.get_color_frame()
-
-    # Convert images to numpy arrays
-    depth_image = np.asanyarray(depth_frame.get_data())
-    color_image = np.asanyarray(color_frame.get_data())
-
-    # Apply colormap on depth image (image must be converted to 8-bit per pixel first)
-    depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image, alpha=0.03), cv2.COLORMAP_JET)
-
-    # Stack both images horizontally
-    images = np.hstack((color_image, depth_colormap))
-
-    # Show images
-    cv2.imshow('RealSense', images)
-
-    # Save images
-    cv2.imwrite("4000LUX_10cm_-15deg.jpg", images)
-
-    # Stop streaming
-    pipeline.stop()
-
-for i in range(3):
-    photo_status("4000LUX_10cm_-15deg")
-    cv2.waitKey(0)
+    # file name
     
+    try:
+        while True:
+            # Change the status
+            change_status()
+            fileName=numbers[numbers_index]+"_"+distance[distance_index]+"_"+degree[degree_index]+".png"
+            # show the status
+            photo_status(fileName)
+
+            # Wait for a coherent pair of frames: depth and color
+            frames = pipeline.wait_for_frames()
+            color_frame = frames.get_color_frame()
+            if not color_frame:
+                continue
+
+            # Convert images to numpy arrays
+            color_image = np.asanyarray(color_frame.get_data())
+
+            # Show images
+            cv2.imshow('RealSense', color_image)
+            cv2.waitKey(1)
+
+            # Save images
+            if keyboard.is_pressed('space'):
+                cv2.imwrite(fileName, color_image)
+
+            # Quit
+            if keyboard.is_pressed('esc'):
+                break
 
 
+    finally:
+        pipeline.stop()
+        
+
+
+capture_photo()
 cv2.destroyAllWindows()
